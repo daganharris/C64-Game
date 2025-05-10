8658f0eaaaf42e153f6f58c7f0e3637232438b9b
Dagan Harris <daganhx@gmail.com>
2025-05-10 21:14:45 +0100

    Third Sprite, Collision Done

--- src/game.c
+++ src/game.c
@@ -9,6 +9,8 @@
 #include <string.h>
 #include "game.h"
 
+#define SPRITE_BASE_ADDR 0x2000
+
 #define SCREEN_WIDTH 320
 #define SCREEN_HEIGHT 200
 
@@ -26,24 +28,34 @@ const char spr1Img[64] = {
 const char spr2Img[64] = {
     24,0,0,24,0,0,60,0,0,60,0,0,124,0,0,126,0,0,126,0,0,127,0,0,127,0,0,127,128,0,127,192,0,63,224,0,63,240,0,63,248,0,31,254,0,31,255,128,15,255,128,7,255,240,1,255,248,0,255,252,0,63,255
 };
-char * const Screen = (char *)0x0400;
-char * const SpriteMem1 = (char *)0x0380;
-char * const SpriteMem2 = (char *)0x03C0;
+const char spr3Img[64] = {
+    15,153,240,29,219,248,51,255,252,103,255,254,79,255,254,223,255,255,176,254,31,230,124,207,230,124,207,240,254,31,255,255,255,255,255,255,255,255,253,255,255,249,255,255,243,127,255,246,63,255,228,63,255,140,31,191,56,15,99,240,3,193,224
+};
 
+char * const Screen = (char *)0x0400;
+char * const SpriteMem1 = (char *)(SPRITE_BASE_ADDR + 0);
+char * const SpriteMem2 = (char *)(SPRITE_BASE_ADDR + 64);
+char * const SpriteMem3 = (char *)(SPRITE_BASE_ADDR + 128);
+char * const SpriteMem4 = (char *)(SPRITE_BASE_ADDR + 192);
+char * const SpriteMem5 = (char *)(SPRITE_BASE_ADDR + 256);
+char * const SpriteMem6 = (char *)(SPRITE_BASE_ADDR + 320);
+char * const SpriteMem7 = (char *)(SPRITE_BASE_ADDR + 384);
+char * const SpriteMem8 = (char *)(SPRITE_BASE_ADDR + 448);
 
 typedef struct 
 {
     char sp;
-    int xpos;
-    int ypos;
-    int xVel; // Horizontal velocity
-    int yVel; // Vertical velocity
+    unsigned short int xpos;
+    unsigned short int ypos;
+    short int xVel; // Horizontal velocity
+    short int yVel; // Vertical velocity
     char color;
     bool show;
 } Player;
 
 Player Blueberry;
 Player Banana;
+Player Apple;
 
 inline void poke(unsigned addr, byte value)
 {
@@ -57,10 +69,10 @@ inline byte peek(unsigned addr)
 
 inline void initsprite(Player *p, int no, char color) {
     p->sp    = no;
-    p->xpos  = 50 + (rand() % (300 - 50 + 1));    // 50 to 300
-    p->ypos  = 75 + (rand() % (200 - 75 + 1));    // 75 to 200
-    p->xVel  = (rand() % 2 == 0) ? 2 : -2;        // +2 or -2
-    p->yVel  = (rand() % 2 == 0) ? 2 : -2;        // +2 or -2
+    p->xpos  = 50 + ((short int)rand() % (300 - 50 + 1));    // 50 to 300
+    p->ypos  = 75 + ((short int)rand() % (200 - 75 + 1));    // 75 to 200
+    p->xVel  = ((short int)rand() % 2 == 0) ? 2 : -2;        // +2 or -2
+    p->yVel  = ((short int)rand() % 2 == 0) ? 2 : -2;        // +2 or -2
     p->color = color;
     p->show  = true;
 }
@@ -88,7 +100,24 @@ inline void updatesprite(Player *p) {
         if (p->ypos > BOTTOM_BORDER) p->ypos = BOTTOM_BORDER;
     }
 
+
     spr_move(p->sp, p->xpos, p->ypos);
+}
+
+inline bool isColliding(Player *a, Player *b) {
+    return !(a->xpos + SPRITE_WIDTH  < b->xpos ||
+             a->xpos > b->xpos + SPRITE_WIDTH  ||
+             a->ypos + SPRITE_HEIGHT < b->ypos ||
+             a->ypos > b->ypos + SPRITE_HEIGHT);
+}
+
+void handleCollision(Player *p1, Player *p2) {
+    if (isColliding(p1, p2)) {
+        p1->xVel = -p1->xVel;
+        p1->yVel = -p1->yVel;
+        p2->xVel = -p2->xVel;
+        p2->yVel = -p2->yVel;
+    }
 }
 
 inline void gameloop() {
@@ -96,10 +125,19 @@ inline void gameloop() {
     makesprite(&Blueberry, spr1Img, SpriteMem1);
     initsprite(&Banana, 1, 7);
     makesprite(&Banana, spr2Img, SpriteMem2);
+    initsprite(&Apple, 2, 5);
+    makesprite(&Apple, spr3Img, SpriteMem3);
+
 
     while (true) {        
         updatesprite(&Blueberry);
         updatesprite(&Banana);
+        updatesprite(&Apple);
+
+        handleCollision(&Blueberry, &Banana);
+        handleCollision(&Blueberry, &Apple);
+        handleCollision(&Banana, &Apple);
+
         vic_waitFrame();
     }
 }
