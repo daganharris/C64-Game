fab8f75cb9071fb727dc3b7fb9850d8d0417ffa3
Jayden Thompson <jaydenthompson08@protonmail.ch>
2025-05-11 15:35:40 +0100

    Resolved #5. See resources/conversion/output.txt

--- resources/conversion/main.py
+++ resources/conversion/main.py
@@ -14,14 +14,16 @@ width, height = image.size
 # Open the output file for writing
 output_file_path = 'output.txt'  # Change this to your desired output file path
 with open(output_file_path, 'w') as output_file:
+    output_file.write('{    ')
     # Iterate over each pixel
     for y in range(height):
+        output_file.write('{')
         for x in range(width):
             pixel = image.getpixel((x, y))
             # 1 for white, 0 for black (in a 1-bit image, pixel is either 0 or 255)
             output_file.write('1' if pixel == 255 else '0')
             output_file.write(',')
         # Newline after each row
-        output_file.write('\n')
-
-print(f"Process complete! Check the output file: {output_file_path}")
+        output_file.write('}\n')
+    output_file.write('}')
+print(f"output file: {output_file_path}")
